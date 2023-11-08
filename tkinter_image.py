9013fcb566d4815b558762e64bb1183f449642a4
Eneo Bovino <eneobov@hotmail.com>
2023-11-08 20:54:58 -0300

    primeira imagem

---
+++ tkinter_image.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+
+img = Image.open('meme_1.jpg')
+
+img_tk = ImageTk.PhotoImage(img)
+
+img_label = Label(root, image=img_tk)
+
+img_label.pack()
+
+root.mainloop()
